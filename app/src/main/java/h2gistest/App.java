6de380d0b14b23b0f2eee55cc30a49d5c17f6244
edigonzales <edi.gonzales@gmail.com>
2021-03-11 15:51:54 +0100

    wip

--- app/src/main/java/h2gistest/App.java
+++ app/src/main/java/h2gistest/App.java
@@ -8,14 +8,17 @@ public class App {
 
     public static void main(String[] args) {
 
-        String h2FileName = new File("src/data/stromversorgungssicherheit").getAbsolutePath();
+        String h2FileName = new File("/Users/stefan/sources/awa_stromversorgungssicherheit/foobar_tmp").getAbsolutePath();
         String url = "jdbc:h2:" + h2FileName;
         try (Connection conn = DriverManager.getConnection(url); Statement stmt = conn.createStatement()) {
+            stmt.execute("CREATE ALIAS IF NOT EXISTS H2GIS_SPATIAL FOR \"org.h2gis.functions.factory.H2GISFunctions.load\"");
+            stmt.execute("CREATE ALIAS IF NOT EXISTS H2GIS_UNLOAD FOR \"org.h2gis.functions.factory.H2GISFunctions.unRegisterH2GISFunctions\"");
+            stmt.execute("CALL H2GIS_SPATIAL();");
+            stmt.execute("SELECT 1");
+            stmt.execute("CALL SHPRead('/Users/stefan/sources/h2gis-tests/app/src/data/netz7.shp', 'netz7')");
+            stmt.execute("CALL H2GIS_SPATIAL();");
 
-            stmt.execute("CALL SHPREAD('/Users/stefan/sources/h2gis-tests/app/src/data/netz7.shp', 'netz7')");
-
-
-
+            //stmt.execute("CALL FILE_TABLE('/Users/stefan/sources/h2gis-tests/app/src/data/netz7.shp', 'netz7')");
 //            while (rs.next()) {
 //                System.out.println(rs.getString(1));
 //                System.out.println(rs.getString(2));
@@ -25,6 +28,8 @@ public class App {
             e.printStackTrace();
             System.out.println(e.getMessage());
         }
+
+
         System.out.println("Hallo Welt.");
     }
 }
